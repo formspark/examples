449045f6a07788ba2b0907f35bce118be89a515e
Bjorn Krols <krols.bj@gmail.com>
2020-11-22 15:10:10 +0100

    Adds Gridsome exampleÃ

---
+++ packages/gridsome/src/pages/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <form method="POST" :action="formActionUrl">
+    <textarea name="message" placeholder="Message" />
+    <button type="submit">Send</button>
+  </form>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Contact",
+  },
+  data() {
+    return {
+      formActionUrl: "https://submit-form.com/your-form-id",
+    };
+  },
+};
+</script>
